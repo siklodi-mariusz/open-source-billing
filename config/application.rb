ecbd135dd0673d85641863ff568e6909254a93ab
siklodi-mariusz <9211589+siklodi-mariusz@users.noreply.github.com>
2020-01-18 23:08:45 +0200

    Make RSpec suite runnable again

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Osb
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     #config.active_record.whitelist_attributes = true
+
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
